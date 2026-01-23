543fc059ccc3f5c40de46707e77ed346489faaf6
Renée <12030657+fixedsyser@users.noreply.github.com>
2026-01-23 11:59:42 +0100

    expand visualizer, split functions

--- visualizer.js
+++ visualizer.js
@@ -1,14 +1,26 @@
 import { createCanvas } from 'canvas';
+import { readFile } from 'fs/promises';
+
+let colormap = JSON.parse(await readFile("colormap.json", "utf8"));
 
 export function generateGarden(req, res){
 
     // const value = parseInt(req.query.value) || 0;
-
-    const canvas = createCanvas(400, 400);
-    const ctx = canvas.getContext('2d');
-    
-    ctx.fillStyle = '#492f1a';
-    ctx.fillRect(0, 0, 400, 400);
+    let canvas = initNewGarden();
 
     canvas.createPNGStream().pipe(res);
 };
+
+
+function initNewGarden(){
+    const canvas = createCanvas(512, 512);
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = getRgba(colormap.base);
+    ctx.fillRect(0, 0, 512, 512);
+    console.log("Welcome in your new garden, take care of it well!")
+    return canvas;
+}
+
+function getRgba(item){
+    return `rgba(${item[0]}, ${item[1]}, ${item[2]})`
+}
